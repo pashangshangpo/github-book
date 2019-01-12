99e6631a6ad24426d2974c2fdf64d0e1bd89e2ed
pashangshangpo <pashangshangpo@gmail.com>
2019-01-13 00:45:12 +0800

    fix: 修复 pathname 没有加上

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { GetData } from '$common/local-storage'
 
-let { href, protocol, host } = location
+let { href, protocol, pathname, host } = location
 
 if (!GetData('github') && href.indexOf('/#/login') === -1) {
-  location.href = `${protocol}//${host}/#/login`
+  location.href = `${protocol}//${host}${pathname}#/login`
   location.reload(true)
 } else {
   require('./main')
