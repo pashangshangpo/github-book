f889cda87a0237c8f79c7707b7f74142f30a953e
pashangshangpo <pashangshangpo@gmail.com>
2019-01-12 16:02:42 +0800

    chore: 导出路径

--- src/common/github.js
+++ src/common/github.js
@@ -3,5 +3,6 @@ import { GetData } from '$common/local-storage'
 
 const { token, projectPath } = GetData('github')
 
+export const ProjectPath = projectPath
 export const Client = Github.client(token)
-export const Repo = Client.repo(projectPath)
+export const Repo = Client.repo(ProjectPath)
